ce9532accb27844752b272cf38e7860471e6cbab
wirunchana <wirunchana.pair@gmail.com>
2021-03-28 01:47:00 +0700

    Add SQLite

---
+++ seting_sqlite/seting_sqlite/DataAccess.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seting_sqlite
+{
+    class DataAccess
+    {
+        public async static void InitializeDatabase()
+        {
+            //await ApplicationData.Current.LocalFolder.CreateFileAsync("sqliteSample.db", CreationCollisionOption.OpenIfExists);
+            //string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            using (SqliteConnection db =
+               new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
+                    "Text_Entry NVARCHAR(2048) NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+
+        public static void AddData(string inputText)
+        {
+            using(SqliteConnection db =
+                 new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @Entry)"; //ใส่ให้ตครบทุกตัวที่ส่งเข้าไป
+                insertCommand.Parameters.AddWithValue("@Entry",inputText); //parameter ที่ต้องการส่งไป DB ต้องมี @ เพื่อป้องกันการใส่ SQL ถ้าเป็น SQL จะไม่ทำงาน
+
+                insertCommand.ExecuteReader(); //Insert แล้วจบการทำงาน ไม่มี Database Return
+                db.Close();
+            }
+        }
+
+        public static List<String> GetData()
+        {
+            List<String> entries = new List<string>();
+
+            using (SqliteConnection db =
+                new SqliteConnection("Filename = sqliteSample.db"))
+            {
+                db.Open();
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT Text_Entry from MyTable", db);
+                SqliteDataReader query = selectCommand.ExecuteReader(); //รอ Database Return เลยต้องมี SqliteDataReader
+
+                while (query.Read()) //กระโดดไปข้อมูลถัดไป ถ้ามี return true, ก้าวต่อไม่ได้หรือไม่มีข้อมูล return false
+                {
+                    entries.Add(query.GetString(0));
+                }
+
+                db.Close();
+            }
+
+            return entries;
+        }
+
+    }
+}
---
+++ seting_sqlite/seting_sqlite/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace seting_sqlite
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataAccess.InitializeDatabase();
+            //DataAccess.AddData("Miss. Mali Jaidee");
+        }
+
+        private void showAllBtn_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> responseData = DataAccess.GetData();
+            string response = string.Empty;
+            foreach (string text in responseData)
+            {
+                response += text + "\n";
+            }
+            MessageBox.Show(response);
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataAccess.AddData(inputTxt.Text);
+            inputTxt.Text = string.Empty;
+        }
+
+    }
+}
